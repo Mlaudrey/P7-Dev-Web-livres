c8b560d5df7aab3137f10bbbe78203ee2f39ec5f
AudreyMl <audreytraining33@gmail.com>
2024-11-11 21:58:29 +0100

    Thing dans app

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const app = express();
+const Thing = require('./models/Thing');
 
 mongoose.connect('mongodb+srv://audrey:mlKjyT@cluster0.oxi2m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     { useNewUrlParser: true,
       useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+const app = express();
 
 app.use(express.json());
 
@@ -19,36 +23,37 @@ app.use((req, res, next) => {
   });
  
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Livre créé !'
-    });
+    delete req.body._id;
+    const thing = new Thing({
+      ...req.body
+   });
+   thing.save()
+    .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
+    .catch(error => res.status(400).json({ error}));
 });
 
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Livre modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+  app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Livre supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+      .then(thing => res.status(200).json(thing))
+      .catch(error => res.status(404).json({ error }));
+  });
+
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oiuytr',
-            title: 'mon premier livre',
-            author: 'Les infos du premier livre',
-            imageUrl:'',
-            year:'',
-            genre:'',
-            rating:'',
-            avarageRating:'',
-        },
-        {
-            _id: 'oiuytrteb',
-            title: 'mon deuxième livre',
-            author: 'Les infos du deuxième livre',
-            imageUrl:'',
-            year:'',
-            genre:'',
-            rating:'',
-            avarageRating:'',
-        },
-    ];
-    res.status(200).json(stuff);
+    Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
 });
 
 module.exports= app;
