638fdd4e8f354291b1e63086e057711d3cf69fca
AudreyMl <audreytraining33@gmail.com>
2024-11-22 16:16:41 +0100

    suppression header et footer dans la page de connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {
-  BrowserRouter, Route, Routes, useLocation,
+  Route, Routes, useLocation,
 } from 'react-router-dom';
 import SignIn from './pages/SignIn/SignIn';
 import Home from './pages/Home/Home';
@@ -22,20 +22,18 @@ function App() {
     setUser(connectedUser);
   }, [connectedUser]);
   return (
-    <BrowserRouter>
-      <div>
-        <ScrollToTop />
-        {location.pathname === APP_ROUTES.SIGN_IN ? null : <Header user={user} setUser={setUser} />}
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path={APP_ROUTES.SIGN_IN} element={<SignIn setUser={setUser} />} />
-          <Route path={APP_ROUTES.BOOK} element={<Book />} />
-          <Route path={APP_ROUTES.UPDATE_BOOK} element={<UpdateBook />} />
-          <Route path={APP_ROUTES.ADD_BOOK} element={<AddBook />} />
-        </Routes>
-        {location.pathname === APP_ROUTES.SIGN_IN ? null : <Footer />}
-      </div>
-    </BrowserRouter>
+    <div>
+      <ScrollToTop />
+      {location.pathname === APP_ROUTES.SIGN_IN ? null : <Header user={user} setUser={setUser} />}
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path={APP_ROUTES.SIGN_IN} element={<SignIn setUser={setUser} />} />
+        <Route path={APP_ROUTES.BOOK} element={<Book />} />
+        <Route path={APP_ROUTES.UPDATE_BOOK} element={<UpdateBook />} />
+        <Route path={APP_ROUTES.ADD_BOOK} element={<AddBook />} />
+      </Routes>
+      {location.pathname === APP_ROUTES.SIGN_IN ? null : <Footer />}
+    </div>
   );
 }
 
